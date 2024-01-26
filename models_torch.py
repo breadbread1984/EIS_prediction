1c8db2ad326d4a755346f46e31e3276be724f806
breadbread1984 <breadbread1984@163.com>
2024-01-26 10:13:40 +0800

    update

--- models_torch.py
+++ models_torch.py
@@ -7,9 +7,9 @@ import torch.nn.functional as F
 class Encoder(nn.Module):
   def __init__(self, seq_len, dict_size = 1024, drop_rate = 0.1):
     super(Encoder, self).__init__()
-    self.layernorm1 = nn.LayerNorm([2, seq_len])
-    self.layernorm2 = nn.LayerNorm([64, seq_len])
-    self.layernorm3 = nn.LayerNorm([128, seq_len])
+    self.layernorm1 = nn.LayerNorm([seq_len, 2])
+    self.layernorm2 = nn.LayerNorm([seq_len, 64])
+    self.layernorm3 = nn.LayerNorm([seq_len, 128])
     self.linear1 = nn.Linear(2, 64)
     self.linear2 = nn.Linear(64, 128)
     self.linear3 = nn.Linear(128, dict_size)
@@ -17,20 +17,21 @@ class Encoder(nn.Module):
     self.dropout2 = nn.Dropout(drop_rate)
     self.gelu1 = nn.GELU()
     self.gelu2 = nn.GELU()
-    self.softmax = nn.Softmax(dim = dict_size)
   def forward(self, inputs):
+    # inputs.shape = (batch, 2, seq)
+    results = torch.transpose(inputs, 1, 2) # results.shape = (batch, seq, 2)
     results = self.layernorm1(inputs)
-    results = self.linear1(results)
+    results = self.linear1(results) # results.shape = (batch, seq, 64)
     results = self.gelu1(results)
     results = self.dropout1(results)
     results = self.layernorm2(results)
-    results = self.linear2(results)
+    results = self.linear2(results) # results.shape = (batch, seq, 128)
     results = self.gelu2(results)
     results = self.dropout2(results)
     results = self.layernorm3(results)
-    results = self.linear3(results)
-    results = self.softmax(results)
-    results = torch.argmax(results, dim = 1)
+    results = self.linear3(results) # results.shape = (batch, seq, dict_size)
+    results = F.softmax(results, dim = -1) # results.shape = (batch, seq, dict_size)
+    results = torch.argmax(results, dim = -1) # results.shape = (batch, seq)
     return results
 
 class Decoder(nn.Module):
@@ -38,28 +39,30 @@ class Decoder(nn.Module):
     super(Decoder, self).__init__()
     self.dict_size = dict_size
 
-    self.layernorm1 = nn.LayerNorm([dict_size, seq_len])
-    self.layernorm2 = nn.LayerNorm([128, seq_len])
-    self.layernorm3 = nn.LayerNorm([64, seq_len])
+    self.layernorm1 = nn.LayerNorm([seq_len, dict_size])
+    self.layernorm2 = nn.LayerNorm([seq_len, 128])
+    self.layernorm3 = nn.LayerNorm([seq_len, 64])
     self.linear1 = nn.Linear(dict_size, 128)
     self.linear2 = nn.Linear(128, 64)
-    self.linear3 = nn.Linear(62, 2)
+    self.linear3 = nn.Linear(64, 2)
     self.dropout1 = nn.Dropout(drop_rate)
     self.dropout2 = nn.Dropout(drop_rate)
     self.gelu1 = nn.GELU()
     self.gelu2 = nn.GELU()
   def forward(self, inputs):
+    # inputs.shape = (batch, seq_len)
     results = F.one_hot(inputs, self.dict_size) # results.shape = (batch, seq_len, dict_size)
     results = self.layernorm1(results)
-    results = self.linear1(results)
+    results = self.linear1(results) # results.shape = (batch, seq_len, 128)
     results = self.gelu1(results)
     results = self.dropout1(results)
     results = self.layernorm2(results)
-    results = self.linear2(results)
+    results = self.linear2(results) # results.shape = (batch, seq_len, 64)
     results = self.gelu2(results)
     results = self.dropout2(results)
     results = self.layernorm3(results)
-    results = self.linear3(results)
+    results = self.linear3(results) # results.shape = (batch, seq_len, 2)
+    results = torch.transpose(results, 1, 2) # results.shape = (batch, 2, seq_len)
     return results
 
 class AETrainer(nn.Module):
@@ -72,4 +75,22 @@ class AETrainer(nn.Module):
     results = self.decoder(results)
     return results
 
+class SelfAttention(nn.Module):
+  def __init__(self, hiddem_dim = 256, num_heads = 8, use_bias = False, drop_rate = 0.1, is_causal = True):
+    super(SelfAttention, self).__init__()
+    self.num_heads = num_heads
+    self.hidden_dim = hidden_dim
+
+    self.linear1 = nn.Linear(hidden_dim, 3 * hidden_dim, bias = use_bias)
+    self.linear2 = nn.Linear(hidden_dim, hidden_dim, bias = use_bias)
+    self.dropout = nn.Dropout(drop_rate)
+  def forward(self, inputs):
+    # inputs.shape = (batch, hidden_dim, seq_len)
+    results = torch.transpose(inputs, 1, 2) # results.shape = (batch, seq_len, hidden_dim)
+    results = self.linear1(inputs) # results.shape = (batch, seq_len, 3 * hidden_dim)
+    b, s, _ = results.shape
+    results = torch.reshape(results, (b, s, 3, self.num_heads, self.hidden_dim // self.num_heads)) # results.shape = (batch, seq_len, 3, head_num, hidden_dim // head_num)
+    results = torch.permute(results, (0,2,3,1,4)) # results.shape = (batch, 3, head_num, seq_len, hidden_dim // head_num)
+    q,k,v = results[:,0,...], results[:,1,...], results[:,2,...] # shape = (batch, head_num, seq_len, hidden_dim // head_num)
+    qk = torch.matmul(q, torch.transpose(k, 2,3)) * (self.hidden_dim // self.num_heads) ** -0.5 # qk.shape = (batch, head_num, seq_len, seq_len)
 
