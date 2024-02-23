fd9f42cd8f1e87b9bcdafd0755d3a3aabfc47939
breadbread1984 <breadbread1984@163.com>
2024-02-24 07:39:52 +0800

    update

--- models.py
+++ models.py
@@ -2,24 +2,28 @@
 
 import tensorflow as tf
 
-def Trainer(channels = 256, rate = 0.2, layers = 2):
+def Trainer(channels = 256, rate = 0.2, layers = 3):
   inputs = tf.keras.Input((1800, 2)) # pulse.shape = (batch, seq_len, 2)
   results = inputs
+  results = tf.keras.layers.BatchNormalization()(results)
+  results = tf.keras.layers.Dense(channels, use_bias  = False)(results)
+  results = tf.keras.layers.Dropout(rate)(results)
   for i in range(layers):
     # spatial mix
     results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0,2,1)))(results)
     results = tf.keras.layers.LayerNormalization()(results)
-    results = tf.keras.layers.Dense(35,activation=tf.keras.activations.gelu)(results)
+    results = tf.keras.layers.Dense(35, use_bias = False)(results)
     results = tf.keras.layers.Dropout(rate)(results)
     results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0,2,1)))(results)
     # channel mix
     results = tf.keras.layers.LayerNormalization()(results)
-    results = tf.keras.layers.Dense(channels, activation=tf.keras.activations.gelu)(results)
+    results = tf.keras.layers.Dense(channels, use_bias = False)(results)
     results = tf.keras.layers.Dropout(rate)(results)
     results = tf.keras.layers.LayerNormalization()(results)
-    results = tf.keras.layers.Dense(2, activation=tf.keras.activations.gelu)(results)
+    results = tf.keras.layers.Dense(channels, use_bias = False)(results)
     results = tf.keras.layers.Dropout(rate)(results)
   results = tf.keras.layers.Dense(2)(results)
+  results = tf.keras.layers.LayerNormalization()(results)
   return tf.keras.Model(inputs = inputs, outputs = results)
 
 if __name__ == "__main__":
