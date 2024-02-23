43073cd6b13b68623ccb37e5080e94902d31759f
breadbread1984 <breadbread1984@163.com>
2024-02-24 06:43:19 +0800

    update

--- models.py
+++ models.py
@@ -7,22 +7,18 @@ def Trainer(channels = 256, rate = 0.2, layers = 1):
   results = inputs
   for i in range(layers):
     # spatial mix
-    skip = results
     results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0,2,1)))(results)
     results = tf.keras.layers.LayerNormalization()(results)
     results = tf.keras.layers.Dense(35,activation=tf.keras.activations.gelu)(results)
     results = tf.keras.layers.Dropout(rate)(results)
     results = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (0,2,1)))(results)
-    results = tf.keras.layers.Add()([results, skip])
     # channel mix
-    skip = results
     results = tf.keras.layers.LayerNormalization()(results)
     results = tf.keras.layers.Dense(channels, activation=tf.keras.activations.gelu)(results)
     results = tf.keras.layers.Dropout(rate)(results)
     results = tf.keras.layers.LayerNormalization()(results)
     results = tf.keras.layers.Dense(2, activation=tf.keras.activations.gelu)(results)
     results = tf.keras.layers.Dropout(rate)(results)
-    results = tf.keras.layers.Add()([results, skip])
   results = tf.keras.layerd.Dense(2)(results)
   return tf.keras.Model(inputs = inputs, outputs = results)
 
